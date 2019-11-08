0222291673226620fb2605e17767cf099775f0ab
michvilleneuve <michele.villeneuve@live.fr>
2019-11-07 20:43:32 -0500

    Envoie test 2

---
+++ Programmation1/Chapitre 1/Exercice10.js
@@ -0,0 +1,18 @@
+//Exercice 10
+// Faire l’algorithme qui permet de calculer la facture d’un client.
+// Cette facture contient la date, le montant de chacun des 3 articles achetés,
+// les taxes de vente et le grand total de la facture.
+// Vous demandez à l'usager la date et le montant de chacun des 3 articles achetés.
+// Le montant de TPS est de 5% et la TVQ est de 9.975%.
+// Vous affichez la date, les 2 montants de taxes et le grand total.
+
+var article1
+var article2
+var article3
+var date
+var TPS = 5%;
+var TVQ = 9.975%;
+
+var article1 = prompt ("Entrez le prix de l'article 1");
+var article2 = prompt ("Entrez le prix de l'article 2");
+var article3 = prompt ("Entrez le prix de l'article 3");
