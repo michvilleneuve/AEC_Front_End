bddb548413be92ce1ee435cf5426252a29f4c416
michvilleneuve <michele.villeneuve@live.fr>
2019-11-26 14:42:34 -0500

    envoie ex.22

--- Programmation1/Chapitre 2/Ex21.js
+++ Programmation1/Chapitre 2/Ex21.js
@@ -4,8 +4,27 @@
 
 //Calculer et afficher l’impôt provincial à charger selon le revenu imposable :
 // revenu > 0 et $7000.00 inclus : 16% du revenu imposable
-// > 7000 et $14000.00 inclus : $1120 + 19.5% du reste
-// >14000 et $23000.00 inclus : $2485 + 21.5% du reste
-// >23000 et $50000.00 inclus : $4420 + 24.5% du reste
-// >50000 et + $11035 + 26% du reste.
+// > 7000 et $13999.00 inclus : $1120 + 19.5% du reste
+// >14000 et $22999.00 inclus : $2485 + 21.5% du reste
+// >23000 et $49,999.00 inclus : $4420 + 24.5% du reste
+// >50000 et : $11035 + 26% du reste.
 
+var salaire;
+
+salaire = Number(prompt("Veuillez entrer votre salaire annuel brut : "));
+
+if (salaire >= 0 && salaire <= 6999){
+    document.write("Le montant que vous devrez débourser en impôt est de : " + (salaire * 0.16));
+}
+else if (salaire >= 7000 && salaire <= 13999){
+    document.write("Le montant que vous devrez débourser en impôt est de : " + (salaire * 0.195 + 1120));
+}
+else if (salaire >= 14000 && salaire <= 22999){
+    document.write("Le montant que vous devrez débourser en impôt est de : " + (salaire * 0.215 + 2485));
+}
+else if (salaire >= 23000 && salaire <= 49999){
+    document.write("Le montant que vous devrez débourser en impôt est de : " + (salaire * 0.245 + 4420));
+}
+else if (salaire >= 50000){
+    document.write("Le montant que vous devrez débourser en impôt est de : " + (salaire * 0.26 + 11035));
+}
---
+++ Programmation1/Chapitre 2/Ex22.js
@@ -0,0 +1,7 @@
+// Ex21.js
+// Code par Michèle Villeneuve
+// Date : 209/11/26
+
+//Créez un programme permettant de changer la couleur du texte de la console.
+// Offrez trois choix de couleurs de fond et de texte à l'utilisateur
+// Affichez une ligne de texte de la couleur de fond et de texte choisis par l'utilisateur
