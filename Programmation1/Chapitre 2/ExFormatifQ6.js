ae39736530c0d14cb833cf18a7ffcc36abe09dc8
michvilleneuve <michele.villeneuve@live.fr>
2019-12-01 16:22:21 -0500

    Envoi Q6 formatif

--- Programmation1/Chapitre 2/ExFormatifQ6.js
+++ Programmation1/Chapitre 2/ExFormatifQ6.js
@@ -1,74 +1,64 @@
-
-//     Vous devez compléter un programme qui permet de calculer la facture pour l’achat d’un robot imprimable en 3 dimensions.
-//
-//     L’usager doit sélectionner le contrôleur
-// 55$ pour un RaspBerry Pi
-// 15$ pour un Raspberry Pi Zero
-//
-// L’usager doit décider du nombre de moteurs entre 2 et 24.
-// Un moteur coûte 5$
-//
-// L’usager décide s’il veut une caméra USB ou non
-// La caméra coûte 35$
-//
-// L’usager décide s’il veut une matrice de LEDS pour les yeux
-// La matrice coûte 10$ pour les deux yeux.
-//
-//     Il faut ensuite ajouter 20$ pour le filament 3D.
-//     Il faut ajouter 15$ pour la batterie USB.
-//
-//
-//     RasPiBot aimerait que votre programme affiche
-//
-// un résumé de l’achat
-// ex: “Robot avec Raspberry Pi Zero, 10 moteurs, pas de caméra, pas de matrice de LEDS.”
-// le prix
-// le prix avec la livraison de 15%.
+// ExFormatif.js
+// // Codé par Michèle Villeneuve
+// // Date : 2019/12/01
 
 var controleur;
 var prixControleur;
 var nbMoteur;
 var camUSB;
 var matriceLEDS;
-var prix;
 var prixCamUSB;
 var prixMatriceLEDS;
+var total;
+var totalLivraison;
 
-controleur = Number(prompt("veuillez choisir entre le RaspBerry Pi (1) et le RaspBerry Pi Zero (2) : "));
-nbMoteur = Number(prompt("Veuillez choisir le nombre de moteur désiré (entre 2 et 24) : "));
+controleur = (prompt("veuillez choisir entre le RaspBerry Pi (1) et le RaspBerry Pi Zero (2) : "));
+nbMoteur = prompt("Veuillez choisir le nombre de moteur désiré (entre 2 et 24) : ");
 camUSB = prompt("Voulez-vous une caméra USB ? Répondre par O ou N");
 matriceLEDS = prompt("Voulez-vous une matrice LEDS ? Répondre par O ou N");
-prix = Number((prixControleur + (nbMoteur * 5) + prixCamUSB + prixMatriceLEDS + 15 + 20));
 
-if (controleur === 1){
-    prixControleur = 55;
-    controleur = "Robot avec RaspBerry Pi ";
+//Choix du robot
+
+if (controleur === "1"){
+    prixControleur = Number(55);
+    controleur = "Robot RaspBerry Pi ";
 }
 else {
-    prixControleur = 15;
-    controleur = "Robot avec RaspBerry Pi Zero "
+    prixControleur = Number(15);
+    controleur = "Robot RaspBerry Pi Zero";
 }
 
-if (camUSB.toUpperCase === "O"){
-    prixCamUSB = 35;
-    camUSB = "Avec caméra USB ";
+//Avec ou sans caméra USB
+
+if (camUSB.toUpperCase() === "O"){
+    prixCamUSB = Number(35);
+    camUSB = ", avec caméra USB ";
 }
 else{
-    prixCamUSB = 0;
-    camUSB = " Pas de caméra USB ";
+    prixCamUSB = Number(0);
+    camUSB = ", pas de caméra USB ";
 }
 
-if (matriceLEDS.toUpperCase === "O"){
-    prixMatriceLEDS = 10;
-    matriceLEDS = "Avec matrice LEDS"
+//Avec ou sans matrice LEDS
+
+if (matriceLEDS.toUpperCase() === "O"){
+    prixMatriceLEDS = Number(10);
+    matriceLEDS = ", avec matrice LEDS ";
 }
 else{
-    prixMatriceLEDS = 0;
-    matriceLEDS = "Pas de matrice LEDS ";
+    prixMatriceLEDS = Number(0);
+    matriceLEDS = ", pas de matrice LEDS ";
 }
-document.write(controleur + nbMoteur + " moteurs " + camUSB + matriceLEDS);
-document.write("Prix : " + prix + "$");
-document.write("Prix avec livraison : " + (prix * 0.15) + " $");
+
+//REMARQUE: J'ai assigné les deux variables suivantes à la fin du code puisqu'elles comprennent
+// des variables qui sont assignées durant le code, selon le choix de l'utilisateur, et autrement rien ne fonctionnait.
+//Si c'est une pratique à éviter dis-moi le !
+total = Number(prixControleur + (nbMoteur * 5) + prixCamUSB + prixMatriceLEDS + 15 + 20);
+totalLivraison = Number(total * 0.15 + total );
+
+document.write(controleur + nbMoteur + " moteurs" + camUSB + matriceLEDS);
+document.write("Prix : " + total + " $ ");
+document.write("Prix avec livraison : " + totalLivraison + " $");
 
 
 
