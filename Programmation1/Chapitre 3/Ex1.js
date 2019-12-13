853df1d9f1d62e4fd15ff12a1d811082489e8be9
michvilleneuve <michele.villeneuve@live.fr>
2019-12-12 19:39:24 -0500

    envoi

---
+++ Programmation1/Chapitre 3/Ex1.js
@@ -0,0 +1,8 @@
+//Ex1.js
+//Code par Michèle Villeneuve
+//2019/12/12
+//Affichez 100 fois dans la console « Je maîtrise les boucles! »
+
+for(var i = 0;i <= 100;i++){
+    document.write("Je maîtrise les boucles!");
+}
---
+++ Programmation1/Chapitre 3/Ex2.js
@@ -0,0 +1,14 @@
+//Ex2.js
+//Code par Michèle Villeneuve
+//2019/12/12
+
+//Affichez un décompte de 1000 à 55.
+
+var nb;
+
+nb = 1000;
+
+while(nb =! 55){
+    nb--;
+    document.write(nb);
+}
