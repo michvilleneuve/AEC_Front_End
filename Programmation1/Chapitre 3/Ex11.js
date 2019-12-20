a0712452d2b526fb48b8c965d53217382f053247
michvilleneuve <michele.villeneuve@live.fr>
2019-12-19 22:06:29 -0500

    Envoi

--- Programmation1/Chapitre 3/Ex11.js
+++ Programmation1/Chapitre 3/Ex11.js
@@ -2,3 +2,11 @@
 //Code par Michèle Villeneuve
 //2019/12/19
 
+//Afficher les nombres de 1 à 10 avec le carré et le cube de chacun de ces nombres.
+
+var i;
+
+for (i = 1;i <= 10; i++){
+    document.write("La racine carré est de " + i * i + " ");
+    document.write(". Le cube est de " + i * i * i + "." + "<br>");
+}
---
+++ Programmation1/Chapitre 3/Ex12.js
@@ -0,0 +1,18 @@
+//Ex12.js
+//Code par Michèle Villeneuve
+//2019/12/19
+
+//Le premier jour de l'ouverture de votre compte, vous déposez .01 .
+// À chaque jour pendant 10 jours, vous déposez le double de la veille.
+// Affichez pour chaque jour, le numéro du jour, le montant déposé et le solde de votre compte.
+
+var jour;
+var depot;
+var solde;
+
+depot = Number(0.01);
+
+for (jour = 0;jour <= 10;jour++){
+    depot = Number(0.1);
+
+}
