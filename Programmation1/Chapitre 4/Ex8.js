858a1b58ba84a67a0673f0435818ac60f08fad4c
michvilleneuve <michele.villeneuve@live.fr>
2020-01-16 21:11:39 -0500

    Envoi

--- Programmation1/Chapitre 4/Ex8.js
+++ Programmation1/Chapitre 4/Ex8.js
@@ -6,20 +6,16 @@
 //Lire les noms et notes d’examen d’une classe de 5 élèves.
 // Afficher ceux (noms et notes) qui ont une note supérieure à la moyenne.
 
-var nom;
-var note;
 var tabNom = [];
 var tabNote = [];
 var somme = 0;
-var longueur = tabNote.length;
+var longueur;
 var resultat;
 
 for (var i = 0;i < 5;i++){
-    nom = prompt("Entrez votre nom : ");
-    note = Number(prompt("Entrez votre note : "));
-    tabNom = [nom];
-    tabNote = [note];
-    somme+= tabNote[note];
-    resultat = (somme / longueur);
+    tabNom[i] = prompt("Entrez votre nom : ");
+    tabNote[i] = Number(prompt("Entrez votre note : "));
+    longueur = tabNote.length;
 }
+resultat = Number(tabNote / longueur);
 document.write(resultat);
---
+++ Programmation1/Chapitre 4/Ex9.js
@@ -0,0 +1,7 @@
+//Ex9.js
+//Codé par Michèle Villeneuve
+//2020-01-16
+
+//Lire 10 nombres quelconques et les placer dans un tableau.
+// Par la suite, trouver le plus petit et le plus grand de ces nombres.
+
